3501872cf90dfba693387df3e642e0fa03599cb8
Skif926 <73491216+Skif926@users.noreply.github.com>
2020-11-19 19:34:41 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,106 +1,69 @@
 #include <iostream>
-#include <cstdint >
+#include <memory>
 using namespace std;
 
-class Power{
+class Date{
 private:
-    int Number1;
-    int Number2;
+    int Day,Mounth,Year;
 public:
-    Power(){
-        Number1 = 2;
-        Number2 = 5;
-    }
-    void set(int a1, int a2 ){
-        Number1 = a1;
-        Number2 = a2;
-    }
-    void calculate(){
-        int a3=1;
-        for (int i=0; i<Number2 ;i++){a3 = a3*Number1;};
-        cout << a3<< endl;
-    }
+    Date(int x,int y,int z):Day(x),Mounth(y),Year(z){}
+    Date(){}
+    int getDay(){return Day;}
+    int getMounth(){return Mounth;}
+    int getYear(){return Year;}
+    void setDay(int x){Day=x;}
+    void setMounth(int x){Mounth=x;}
+    void setYear(int x){Year=x;}
+    friend ostream &operator<< (ostream &out, const Date &t);
 };
-class RGBA{
-  private:
-  std::uint8_t m_red,m_green,m_blue,m_alpha;
-public:
-  RGBA(){
-    m_red=0;
-    m_green=0;
-    m_blue=0;
-    m_alpha=255;
-  }
-  RGBA(uint8_t red,uint8_t green,uint8_t blue,uint8_t alpha){
-      m_red=red;
-      m_green=green;
-      m_blue=blue;
-      m_alpha=alpha;
-  }
-  void print(){
-  cout << m_red<<"/"<<m_green<<"/"<<m_blue<<"/"<<m_alpha<< endl;
-  }
-
-};
-class Stack{
-private:
- int s_arr[10],count;
-public:
- void push(int i){
-     if (count <10 && count >=0){
-     s_arr[count]=i;
-     count++;
-     cout <<"true"<<endl;
-     }
-     else {cout <<"false"<<endl;}
- }
- void reset(){
-     count = 0;
-     for(int i=0;i<10;i++){s_arr[i]=0;};
- }
- void print(){
-     cout<<"( ";
-     for(int i=0;i<count;i++){
-         cout<<s_arr[i]<<" ";
-     };
-     cout<<")"<<endl;
- }
- void pop(){
- if (count>0){
-     s_arr[count]=0;
-     count--;
- }
- }
-};
+ostream &operator<< (ostream &out, const Date &t){
+    out<<t.Day<<"."<<t.Mounth<<"."<<t.Year;
+    return out;
+}
 
 
+int More(unique_ptr<Date> &a1,unique_ptr<Date> &a2){
 
+    if (a1->getYear()>a2->getYear())return a1->getDay();
+    else if(a1->getYear()<a2->getYear())return a2->getDay();
+    else {
+        if (a1->getMounth()>a2->getMounth())return a1->getDay();
+        else if (a1->getMounth()<a2->getMounth())return a2->getDay();
+        else {
+            if (a1->getDay()>a2->getDay())return a1->getDay();
+            else return a2->getDay();
+        }
+    }
+}
 
-
+void Change(unique_ptr<Date>&a1,unique_ptr<Date>&a2){
+    unique_ptr<Date>a3(move(a1));
+    unique_ptr<Date>a4(move(a2));
+    a1=move(a4);
+    a2=move(a3);
+}
 
 
 int main()
 {
-    Power Iam;
-    Iam.calculate();
-    RGBA a(1,2,34,4);
-    a.print();
-    Stack stack;
-        stack.reset();
-        stack.print();
+    unique_ptr<Date> today(new Date);
+    today->setDay(1);
+    today->setMounth(1);
+    today->setYear(2000);
+    cout << *today<<endl;
+    unique_ptr<Date> date(move(today));
+    cout<<*date<<endl;
+    if (today)cout<<"+"<<endl;else cout<<"-"<<endl;
+    if (date)cout<<"+"<<endl;else cout<<"-"<<endl;
+    unique_ptr<Date>date1(new Date(1,2,2003)),date2(new Date(15,2,2003));
+    cout<<More(date1,date2)<<endl;
+    cout<<*date1<<endl;
+    cout<<*date2<<endl;
+    Change(date1,date2);
+    cout<<*date1<<endl;
+    cout<<*date2<<endl;
 
-        stack.push(3);
-        stack.push(7);
-        stack.push(5);
-        stack.print();
 
-        stack.pop();
-        stack.print();
 
-        stack.pop();
-        stack.pop();
-        stack.print();
-
-        return 0;
-
-};
+    return 0;
+}
